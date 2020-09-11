11099d7b57ac3fcc4d18993611ce56634b7d43d4
Kyle Mitofsky <Kyle.Mitofsky@vermont.gov>
2020-09-11 00:05:37 -0400

    init commit

---
+++ App_Code/WsdlReflector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+
+namespace WcfService1
+{
+    public class WsdlReflector : SoapExtensionReflector
+    {
+        public override void ReflectMethod()
+        {
+            var x = "hi'";
+        }
+        public override void ReflectDescription()
+        {
+            Console.WriteLine(ReflectionContext.ServiceDescription);
+            ServiceDescription description = ReflectionContext.ServiceDescription;
+            foreach (Service service in description.Services)
+            {
+                foreach (Port port in service.Ports)
+                {
+                    foreach (ServiceDescriptionFormatExtension extension in port.Extensions)
+                    {
+                        SoapAddressBinding binding = extension as SoapAddressBinding;
+                        if (null != binding)
+                        {
+                            //binding.Location = binding.Location.Replace("http://mywebservice.comp.com", "https://uat.mywebservice.com");
+                            binding.Location = "https://new.new";
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ IService1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+    }
+
+
+    [DataContract]
+    public class CompositeType
+    {
+        [DataMember]
+        public bool BoolValue { get; set; } = true;
+
+        [DataMember]
+        public string StringValue { get; set; } = "Hello ";
+    }
+}
